a5a43811f318bb1d3046f1ff95de6df74541c76d
cae018 <cae018@bucknell.edu>
2017-03-21 17:47:25 -0400

    Add files via upload

---
+++ csci204project/csci204Project-master/src/plot.py
@@ -0,0 +1,85 @@
+"""
+This file contains objects related to our plotting
+"""
+
+
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
+
+class MyPlot:
+    """
+    This class will be used for all plotting.
+    In most cases, the functions will just be static
+    However, this allows for on nice interface
+    """
+
+    def __init__(self):
+        """
+        constructor
+        """
+        pass
+
+
+    @staticmethod
+    def twoDScatter(xList, yList, xLabels=None, yLabels=None):
+        '''
+        Pre: xList, yList are lists of integers. xLabels, yLabels are strings
+        Post: Returns a scatter plot with xList, yList containing x and y values
+        '''
+        #Get x and y values
+        y = yList
+        x = xList
+        width = 25
+        plt.scatter(x, y, width, color="blue")
+        
+        fig = plt.gcf()
+        #Make axis labels
+        plt.xlabel(xLabels)
+        plt.ylabel(yLabels)
+    
+        #Produce graph
+        plt.tight_layout()
+        plt.show()
+
+
+    @staticmethod
+    def twoDBar(xList, yList, xLabels=None, yLabels=None):
+        '''
+        Pre: xList, yList are lists of integers. xLabels, yLabels are strings
+        Post: Returns a bar graph with xList, yList containing x and y (height) values
+        '''
+        #Get x and y values
+        y = yList
+        x = xList
+        width = 1/1.5
+        plt.bar(x, y, width, color="blue")
+        
+        
+        fig = plt.gcf()
+        #Make axis labels
+        plt.xlabel(xLabels)
+        plt.ylabel(yLabels)
+    
+        #Produce grap
+        plt.tight_layout()
+        plt.show()
+
+
+
+
+
+def testMyPlot():
+    """
+    Used to test MyPlot class
+    """
+    d = MyPlot
+    d.twoDBar([5,3,4,9],[10,4,5,8],"X-axis","Y-Axis")
+    d.twoDScatter([5,3,4,9],[10,4,5,8],"X-axis","Y-Axis")
+
+
+if __name__ == "__main__":
+    testMyPlot()
+
+
